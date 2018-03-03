45ac88128ea50069cef3b99456bda260e2f0afb5
Credwa <craigroe7@gmail.com>
2018-03-03 03:25:21 -0500

    change to port 80

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 80;
 let app = express();
 
 // app.use(bodyParser.json());
